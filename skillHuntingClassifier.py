47a716c2695ac70c58125ba09faa3a5c60d5f92f
gokcegok <53365726+gokcegok@users.noreply.github.com>
2023-05-02 03:33:24 +0300

    Added comments

--- skillHuntingClassifier.py
+++ skillHuntingClassifier.py
@@ -1,70 +1,64 @@
 # -*- coding: utf-8 -*-
 """
-SCOUTIUM
+Skill Hunting Classification using Machine Learning Methods
 
-Makine Ogrenmesi ile Yetenek Avciligi Siniflandirma
 
-İs Problemi: 
+Business Problem:
     
-    Scout’lar tarafından izlenen futbolcuların 
-    özelliklerine verilen puanlara göre, 
-    oyuncuların hangi sınıf (average, highlighted) 
-    oyuncu olduğunun tahminlemesi
+    Predicting which class (average, highlighted) the players are in 
+    according to the scores given to the characteristics of 
+    the football players watched by the Scouts.
     
-    Scout: Gözlemci ya da yetenek avcısı, 
-    gelecek vadettiği düşünülen sporcuları 
-    gözlemleyerek mevcut yeteneklerini ve 
-    potansiyellerini tespit eden uzman kişi
 Created on Tue May  2 02:08:33 2023
 
 @author: gokcegok
 """
 
-# %% Libraries
+# %% LIBRARIES
 
 import pandas as pd
 import eda
-from sklearn.preprocessing
 
 
-# %% Dataset
+# %% DATASET
 
 # --------------------------------------------------------------------------- #
-# Veri seti Scoutium’dan maçlarda gözlemlenen futbolcuların özelliklerine göre 
-# scoutların değerlendirdikleri futbolcuların, maç içerisinde puanlanan 
-# özellikleri ve puanlarını içeren bilgilerden oluşmaktadır.
+# The data set consists of information from Scoutium, which includes 
+# the features and scores of the football players evaluated by the scouts 
+# according to the characteristics of the footballers observed in the matches.
 # --------------------------------------------------------------------------- #
 
 attributes = pd.read_csv("scoutium_attributes.csv", sep=";")
-
-# Degiskenler
-
-# task_response_id: Bir scoutun bir maçta bir takımın kadrosundaki 
-# tüm oyunculara dair değerlendirmelerinin kümesi
-# match_id: İlgili maçın id'si
-# evaluator_id: Değerlendiricinin(scout'un) id'si
-# player_id: İlgili oyuncunun id'si
-# position_id: İlgili oyuncunun o maçta oynadığı pozisyonun id’si
-# 1: Kaleci, 2: Stoper, 3: Sağ bek, 4: Sol bek, 
-# 5: Defansif orta saha, 6: Merkez orta saha, 7: Sağ kanat, 
-# 8: Sol kanat, 9: Ofansif orta saha, 10: Forvet
-# analysis_id: Bir scoutun bir maçta bir oyuncuya dair 
-# özellik değerlendirmelerini içeren küme
-# attribute_id: Oyuncuların değerlendirildiği her bir özelliğin id'si
-# attribute_value: Bir scoutun bir oyuncunun bir özelliğine verdiği değer(puan)
-
 potential_labels = pd.read_csv("scoutium_potential_labels.csv", sep=";")
 
-# task_response_id: Bir scoutun bir maçta bir takımın kadrosundaki 
-# tüm oyunculara dair değerlendirmelerinin kümesi
-# match_id: İlgili maçın id'si
-# evaluator_id: Değerlendiricinin(scout'un) id'si
-# player_id: İlgili oyuncunun id'si
-# potential_label: Bir scoutun bir maçta bir oyuncuyla ilgili 
-# nihai kararını belirten etiket. (hedef değişken)
+# VARIABLES
+
+# task_response_id: The id of the set of a scout's evaluations of all players 
+# on a team's roster in a match
+
+# match_id: Match ID
+
+# evaluator_id: Scout ID
+
+# player_id: Player ID
+
+# position_id: The id of the position played by the relevant player in that match
+# 1: Goalstopper, 2: Stoper, 3: Right Back, 4: Left Back, 
+# 5: Defensive Midfielder, 6: Center Midfielder, 7: Right Winger, 
+# 8: Left Winger, 9: Offensive Midfielder, 10: Forward
+
+# analysis_id: The id of the set containing a scout's attribute evaluations 
+# of a player in a match
+
+# attribute_id: The id of each attribute the players were evaluated on
+
+# attribute_value: Value (points) given by a scout to a player's attribute
+
+# potential_label: Label indicating the final decision of a scout 
+# regarding a player in a match. (target variable)
 
 
-# Merging datasets
+# MERGING DATASETS
 
 data_ = pd.merge(attributes, potential_labels, how="left",
                 on = ['task_response_id', 'match_id', 
@@ -73,9 +67,8 @@ data_ = pd.merge(attributes, potential_labels, how="left",
 data = data_.copy()
 
 eda.check_df(data)
-# %%
-
-# Dropping "Kaleci"(goalkepper) in position_id column
+# %% 
+# Dropping "1" (Goalkepper) in position_id column
 
 data = data[data["position_id"] != 1]
 
@@ -87,7 +80,7 @@ print(data["potential_label"].value_counts())
 
 data = data[data["potential_label"] != "below_average"]
 
-# %%
+# %% DATA PREPROCESSING
 
 # Pivot table with 
 # "player_id", "position_id" and "potential_label" in the index, 
@@ -95,44 +88,14 @@ data = data[data["potential_label"] != "below_average"]
 # the score given by the scouts to the players "attribute_value" in the values
 
 
-pivot = pd.pivot_table(data, values='attribute_value', 
+data = pd.pivot_table(data, values='attribute_value', 
                        index=["player_id", "position_id", "potential_label"],
                        columns=["attribute_id"])
 
-pivot.reset_index(inplace=True)
+data.reset_index(inplace=True)
 
-pivot.columns = pivot.columns.astype("str")
+data.columns = data.columns.astype("str")
 
 # we now have a dataset with each row holding 
 # the points awarded to a player by the scouts, 
 # the player's position and the target variable!
-
-# %%
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
